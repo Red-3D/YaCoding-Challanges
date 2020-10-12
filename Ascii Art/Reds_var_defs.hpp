f7538512d6d45484caa0ffb3b8b72d00efd0bafd
Red_3D <42870774+Red-3D@users.noreply.github.com>
2020-10-12 17:26:34 +0200

    Add files via upload

---
+++ Ascii Art/Reds_var_defs.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef _RedsVarDefs_
+#define _RedsVarDefs_
+#endif
+
+typedef unsigned char			uchar;
+typedef unsigned short int		ushort;
+typedef unsigned int			uint;
+typedef unsigned long int		ulong;
+typedef unsigned long long int	ulonglong;
+
+typedef	char		i8;
+typedef short		i16;
+typedef	int			i32;
+typedef long long	i64;
+
+typedef unsigned char		u8;
+typedef unsigned short		u16;
+typedef unsigned int		u32;
+typedef unsigned long long	u64;
